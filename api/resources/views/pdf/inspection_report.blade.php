1cea07bcf695eafcbdce6778947e1b0d9a48f41f
Andi Muh Hidayat <andimuhhidayat2002@gmail.com>
2026-01-27 17:59:32 +0800

    Finalized T75 PDF Refactoring: Standardized Data Access via AdminController

--- api/resources/views/pdf/inspection_report.blade.php
+++ api/resources/views/pdf/inspection_report.blade.php
@@ -470,28 +470,28 @@
                          <!-- Pressure Gauge -->
                          <tr>
                             <td style="border:1px solid #eee;">Pressure Gauge Condition</td>
-                            <td style="border:1px solid #eee;text-align:center;">{!! badge($inspection->pressure_gauge_condition) !!}</td>
+                            <td style="border:1px solid #eee;text-align:center;">{!! badge($t75Data['instruments']['pressure_gauge']['condition']) !!}</td>
                             <td style="border:1px solid #eee;text-align:center;color:#bbb;">-</td>
                          </tr>
                          <tr>
                             <td colspan="3" style="border:1px solid #eee; padding-left: 15px; font-size: 6pt; color: #555;">
-                                SN: {{ $inspection->pressure_gauge_serial_number ?? '-' }} | 
-                                Cal. Date: {{ $inspection->pressure_gauge_calibration_date ? \Carbon\Carbon::parse($inspection->pressure_gauge_calibration_date)->format('Y-m-d') : '-' }}<br>
-                                Reading (P1): {{ $inspection->pressure_1 ? (float)$inspection->pressure_1.' MPa' : '-' }} {{ $inspection->pressure_1_timestamp ? '('. \Carbon\Carbon::parse($inspection->pressure_1_timestamp)->format('H:i') .')' : '' }}<br>
-                                Reading (P2): {{ $inspection->pressure_2 ? (float)$inspection->pressure_2.' MPa' : '-' }} {{ $inspection->pressure_2_timestamp ? '('. \Carbon\Carbon::parse($inspection->pressure_2_timestamp)->format('H:i') .')' : '' }}
+                                SN: {{ $t75Data['instruments']['pressure_gauge']['sn'] }} | 
+                                Cal. Date: {{ $t75Data['instruments']['pressure_gauge']['cal_date'] }}<br>
+                                Reading (P1): {{ $t75Data['instruments']['pressure_gauge']['p1'] }} {{ $t75Data['instruments']['pressure_gauge']['p1_time'] }}<br>
+                                Reading (P2): {{ $t75Data['instruments']['pressure_gauge']['p2'] }} {{ $t75Data['instruments']['pressure_gauge']['p2_time'] }}
                             </td>
                          </tr>
 
                          <!-- Level Gauge -->
                          <tr>
                             <td style="border:1px solid #eee;">Level Gauge Condition</td>
-                            <td style="border:1px solid #eee;text-align:center;">{!! badge($inspection->level_gauge_condition) !!}</td>
+                            <td style="border:1px solid #eee;text-align:center;">{!! badge($t75Data['instruments']['level_gauge']['condition']) !!}</td>
                             <td style="border:1px solid #eee;text-align:center;color:#bbb;">-</td>
                          </tr>
                          <tr>
                              <td colspan="3" style="border:1px solid #eee; padding-left: 15px; font-size: 6pt; color: #555;">
-                                Reading (L1): {{ $inspection->level_1 ? (float)$inspection->level_1.' %' : '-' }} {{ $inspection->level_1_timestamp ? '('. \Carbon\Carbon::parse($inspection->level_1_timestamp)->format('H:i') .')' : '' }}<br>
-                                Reading (L2): {{ $inspection->level_2 ? (float)$inspection->level_2.' %' : '-' }} {{ $inspection->level_2_timestamp ? '('. \Carbon\Carbon::parse($inspection->level_2_timestamp)->format('H:i') .')' : '' }}
+                                Reading (L1): {{ $t75Data['instruments']['level_gauge']['l1'] }} {{ $t75Data['instruments']['level_gauge']['l1_time'] }}<br>
+                                Reading (L2): {{ $t75Data['instruments']['level_gauge']['l2'] }} {{ $t75Data['instruments']['level_gauge']['l2_time'] }}
                              </td>
                          </tr>
 
