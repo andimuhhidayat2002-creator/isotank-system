3d16653f1a3dbf6bd8049687c31b0277dc508e6d
Andi Muh Hidayat <andimuhhidayat2002@gmail.com>
2026-01-29 20:58:56 +0800

    Fix: Added logging to calibration master update process

--- api/resources/js/app.js
+++ api/resources/js/app.js
@@ -1,22 +1,24 @@
 import 'bootstrap';
 import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs5';
+
+// Import Core Buttons
+import 'datatables.net-buttons';
 import 'datatables.net-buttons-bs5';
+
+// Import Button Features
 import 'datatables.net-buttons/js/buttons.html5.mjs';
 import 'datatables.net-buttons/js/buttons.print.mjs';
+import 'datatables.net-buttons/js/buttons.colVis.mjs';
 
+// Import Export Dependencies
 import JSZip from 'jszip';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
-// Required for DataTables Export
+// Global Assignments for Legacy/Blade Compatibility
+window.$ = window.jQuery = jQuery;
 window.JSZip = JSZip;
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 window.pdfMake = pdfMake;
-
-
-// Expose jQuery globally for legacy scripts in Blade views
-window.$ = window.jQuery = jQuery;
-
-// Expose DataTable globally for Blade scripts
 window.DataTable = DataTable;
